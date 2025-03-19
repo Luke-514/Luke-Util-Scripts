06e0a04290c89bd77b50196d106f24c6723f0a51
Luke514 <42091968+Luke-514@users.noreply.github.com>
2025-03-19 12:13:09 +0900

    Update Check_CPU_and_MEM_Available.sh

--- Linux/Check_CPU_and_MEM_Available.sh
+++ Linux/Check_CPU_and_MEM_Available.sh
@@ -4,6 +4,6 @@ CPUIDLE=`top -b -n 1 | grep Cpu | awk -F "," '{print $4}' | tr -d ' id'`
 MEMAVAILABLE=`free $1 | awk '{print $7}' | grep -Ev ^$`
 SWAPAVAILABLE=`free $1 | grep Swap | awk '{print $4}'`
 
-echo "CPU  IDLE     : "$CPUIDLE
+echo "CPU  Idle     : "$CPUIDLE
 echo "MEM  Available: "$MEMAVAILABLE
 echo "Swap Available: "$SWAPAVAILABLE
