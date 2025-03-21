700c6a2e51f3fbea900890b030bbb79e71b3b15c
Luke514 <42091968+Luke-514@users.noreply.github.com>
2025-03-21 19:58:20 +0900

    Add files via upload

---
+++ Windows/Video_Convert_to_GIF.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Video Width (Height is automatically calculated)
+SET VIDEOWIDTH=640
+
+ffmpeg -version >nul 2>&1
+
+if %errorlevel% neq 0 (
+  echo ffmpeg not found. Start installation...
+  winget install ffmpeg
+) else (
+  if "%~1"=="" (
+      echo Drag and drop video files into this batch file
+      pause
+      exit
+  )
+  SET "FILENAME=%~n1"
+  SET "OUTPUT=%~dp1!FILENAME!"
+
+  ffmpeg -i "%~1" -filter_complex "[0:v] fps=30,scale=!VIDEOWIDTH!:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" "!OUTPUT!".gif
+)
+
+if %errorlevel% neq 0 (
+  pause
+)
