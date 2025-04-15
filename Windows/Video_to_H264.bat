8c6bd8398f1cea1355bfefb3d72b0910ae726797
Luke514 <42091968+Luke-514@users.noreply.github.com>
2025-04-15 23:21:35 +0900

    Add files via upload

---
+++ Windows/Video_to_H264.bat
@@ -0,0 +1,30 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Image quality (0-51, the lower the number, the higher the image quality)
+REM Preset (ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow. Recommend medium or veryfast)
+
+SET IMAGEQUALITY=23
+SET PRESET=veryfast
+
+ffmpeg -version >nul 2>&1
+
+if %errorlevel% neq 0 (
+  echo ffmpeg not found. Start installation...
+  winget install ffmpeg
+) else (
+  if "%~1"=="" (
+      echo Drag and drop video files into this batch file
+      pause
+      exit
+  )
+  SET "FILENAME=%~n1"
+  SET "EXTENSION=%~x1"
+  SET "OUTPUT=%~dp1!FILENAME!_convert!EXTENSION!"
+
+  ffmpeg -i "%~1" -c:v libx264 -crf !IMAGEQUALITY! -preset !PRESET! "!OUTPUT!"
+)
+
+if %errorlevel% neq 0 (
+  pause
+)
