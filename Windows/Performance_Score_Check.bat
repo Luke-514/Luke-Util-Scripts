f462c03ce2ea0567cf2e39220d2f56449c24ee2e
Luke514 <42091968+Luke-514@users.noreply.github.com>
2025-06-01 23:03:37 +0900

    Add files via upload

--- Windows/Performance_Score_Check.bat
+++ Windows/Performance_Score_Check.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell -NoProfile -ExecutionPolicy Unrestricted -Command "Get-CimInstance Win32_WinSat | Select-Object CPUScore, MemoryScore, D3DScore, GraphicsScore, DiskScore, WinSPRLevel | Out-String -Stream | ?{$_ -ne \"\"}"
+powershell -NoProfile -ExecutionPolicy Unrestricted -Command "Get-CimInstance Win32_WinSat | Select-Object CPUScore, MemoryScore, GraphicsScore, DiskScore, WinSPRLevel | Out-String -Stream | ?{$_ -ne \"\"}"
 pause
