166fc5888f58e9ed67ff879d775131d6fd51eca0
Luke514 <42091968+Luke-514@users.noreply.github.com>
2025-11-08 18:31:56 +0900

    Enhance ffmpeg installation process in batch file
    Updated ffmpeg installation command to accept agreements.

--- Windows/Video_to_H264.bat
+++ Windows/Video_to_H264.bat
@@ -11,7 +11,7 @@ ffmpeg -version >nul 2>&1
 
 if %errorlevel% neq 0 (
   echo ffmpeg not found. Start installation...
-  winget install ffmpeg
+  winget install ffmpeg --accept-package-agreements --accept-source-agreements
 ) else (
   if "%~1"=="" (
       echo Drag and drop video files into this batch file
@@ -27,4 +27,5 @@ if %errorlevel% neq 0 (
 
 if %errorlevel% neq 0 (
   pause
+
 )
